a5010032f07ca61204e95fa780ebe90cb7219196
zhangciwu <conanla@qq.com>
2015-07-29 10:40:24 +0800

    登陆 绑定

---
+++ meiLiTianShi/UI/Login/PhoneBindVC.m
@@ -0,0 +1,45 @@
+//
+//  PhoneBindVC.m
+//  meiLiTianShi
+//
+//  Created by zcw on 15/7/29.
+//  Copyright (c) 2015年 Jiayan Technologies Co., Ltd. All rights reserved.
+//
+
+#import "PhoneBindVC.h"
+
+@interface PhoneBindVC ()
+@property (weak, nonatomic) IBOutlet UITextField *phoneInput;
+@property (weak, nonatomic) IBOutlet UIButton *sendCodeButton;
+@property (weak, nonatomic) IBOutlet UITextField *codeInput;
+@property (weak, nonatomic) IBOutlet UIButton *submitButton;
+
+@end
+
+@implementation PhoneBindVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)sendCodeButtonPress:(id)sender {
+}
+- (IBAction)submitButtonPress:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ meiLiTianShi/UI/Login/PhoneLoginVC.m
@@ -0,0 +1,44 @@
+//
+//  PhoneLoginVC.m
+//  meiLiTianShi
+//
+//  Created by zcw on 15/7/29.
+//  Copyright (c) 2015年 Jiayan Technologies Co., Ltd. All rights reserved.
+//
+
+#import "PhoneLoginVC.h"
+
+@interface PhoneLoginVC ()
+@property (weak, nonatomic) IBOutlet UITextField *phoneInput;
+@property (weak, nonatomic) IBOutlet UITextField *passwordInput;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+
+@end
+
+@implementation PhoneLoginVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)loginButtonPress:(id)sender {
+}
+- (IBAction)forgetPasswordPress:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
