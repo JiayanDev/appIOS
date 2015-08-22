56250325cbb7adc0ab3379059e29163b010d07c4
zhangciwu <conanla@qq.com>
2015-08-22 15:31:28 +0800

    share

---
+++ meiLiTianShi/UI/Share/ShareViewManager.m
@@ -0,0 +1,17 @@
+//
+// Created by zcw on 15/8/22.
+// Copyright (c) 2015 Jiayan Technologies Co., Ltd. All rights reserved.
+//
+
+#import "ShareViewManager.h"
+
+
+@implementation ShareViewManager {
+
+}
+
+-(void)showSharePanelOnto:(UIView *)view{
+
+}
+
+@end
