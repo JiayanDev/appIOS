bd7c2516f1c9cd5bb72426708b5df2b976928e3a
zhangciwu <conanla@qq.com>
2015-08-31 15:42:46 +0800

    avatar and name cell

--- meiLiTianShi/frameworks/XLform_additions/AvatarAndNameAndDescCell.m
+++ meiLiTianShi/frameworks/XLform_additions/AvatarAndNameAndDescCell.m
@@ -3,13 +3,17 @@
 // Copyright (c) 2015 Jiayan Technologies Co., Ltd. All rights reserved.
 //
 
+#import <Masonry/View+MASAdditions.h>
 #import "AvatarAndNameAndDescCell.h"
+#import "UILabel+MLStyle.h"
 
 NSString * const XLFormRowDescriptorType_AvatarAndNameAndDescCell = @"XLFormRowDescriptorType_AvatarAndNameAndDescCell";
 
 
 @implementation AvatarAndNameAndDescCell {
-
+    UILabel *nameLabel;
+    UILabel *descLabel;
+    UIImageView *avatarView;
 }
 
 +(void)load
@@ -18,5 +22,35 @@ NSString * const XLFormRowDescriptorType_AvatarAndNameAndDescCell = @"XLFormRowD
 }
 
 
+- (void)configure {
+    avatarView=[UIImageView new];
+    [self.contentView addSubview:avatarView];
+
+    nameLabel=[UILabel newMLStyleWithSize:15 isGrey:NO];
+    [self.contentView addSubview:nameLabel];
+
+    descLabel=[UILabel newMLStyleWithSize:15 isGrey:YES];
+    [self.contentView addSubview:descLabel];
+
+    [avatarView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.contentView).offset(8);
+        make.centerY.equalTo(self.contentView);
+        make.size.mas_equalTo(CGSizeMake(65,65));
+    }];
+
+
+
+
+}
+
+- (void)update {
+
+}
+
+
++ (CGFloat)formDescriptorCellHeightForRowDescriptor:(XLFormRowDescriptor *)rowDescriptor {
+
+    return 110;
+}
 
 @end
