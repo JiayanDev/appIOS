6d6ce30b987c1ecba2f62851cd496045ab207cb7
zhangciwu <conanla@qq.com>
2015-09-10 10:43:22 +0800

    webview

---
+++ meiLiTianShi/UI/Event/GeneralWebVC.h
@@ -0,0 +1,11 @@
+//
+// Created by zcw on 15/9/10.
+// Copyright (c) 2015 Jiayan Technologies Co., Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GeneralWebVC : NSObject
+@property (nonatomic, strong)NSURL *url;
+@end
---
+++ meiLiTianShi/UI/Event/GeneralWebVC.m
@@ -0,0 +1,18 @@
+//
+// Created by zcw on 15/9/10.
+// Copyright (c) 2015 Jiayan Technologies Co., Ltd. All rights reserved.
+//
+
+#import "GeneralWebVC.h"
+
+@interface GeneralWebVC()
+@property (strong, nonatomic)  UIWebView *webView;
+
+@end
+@implementation GeneralWebVC {
+
+}
+
+
+
+@end
