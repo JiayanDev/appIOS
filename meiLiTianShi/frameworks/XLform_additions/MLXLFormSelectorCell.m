c80e905c23fae99b0e5e206708e8a3af207269c4
zhangciwu <conanla@qq.com>
2015-10-12 16:23:13 +0800

    fixing adapting in iphone 5

--- meiLiTianShi/frameworks/XLform_additions/MLXLFormSelectorCell.m
+++ meiLiTianShi/frameworks/XLform_additions/MLXLFormSelectorCell.m
@@ -17,20 +17,30 @@
     [XLFormViewController.cellClassesForRowDescriptorTypes setObject:[MLXLFormSelectorCell class] forKey:XLFormRowDescriptorTypeSelectorPush];
 }
 
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.textLabel.font=[UIFont systemFontOfSize:15];
+
+        [self.imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.centerY.equalTo(self.contentView);
+            make.left.equalTo(self.contentView).offset(16);
+        }];
+
+        [self.textLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.centerY.equalTo(self.contentView);
+            make.left.equalTo(self.imageView.mas_right).offset(16);
+            make.left.equalTo(self.contentView).offset(16).priority(500);
+        }];
+    }
+
+    return self;
+}
+
+
 - (void)configure {
     [super configure];
-    self.textLabel.font=[UIFont systemFontOfSize:15];
 
-    [self.imageView mas_makeConstraints:^(MASConstraintMaker *make) {
-       make.centerY.equalTo(self.contentView);
-        make.left.equalTo(self.contentView).offset(16);
-    }];
-
-    [self.textLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerY.equalTo(self.contentView);
-        make.left.equalTo(self.imageView.mas_right).offset(16);
-        make.left.equalTo(self.contentView).offset(16).priority(500);
-    }];
 }
 
 -(void)update{
@@ -44,6 +54,12 @@
 
     self.detailTextLabel.font=[UIFont systemFontOfSize:15];
     self.detailTextLabel.textColor=THEME_COLOR_TEXT_LIGHT_GRAY;
+
+//    [self.textLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+//        make.centerY.equalTo(self.contentView);
+//        make.left.equalTo(self.imageView.mas_right).offset(16);
+//        make.left.equalTo(self.contentView).offset(16).priority(500);
+//    }];
 //    self.selectionStyle = self.rowDescriptor.isDisabled ? UITableViewCellSelectionStyleNone : UITableViewCellSelectionStyleDefault;
 //    self.textLabel.text = [NSString stringWithFormat:@"%@%@", self.rowDescriptor.title, self.rowDescriptor.required && self.rowDescriptor.sectionDescriptor.formDescriptor.addAsteriskToRequiredRowsTitle ? @"*" : @""];
 //    self.detailTextLabel.text = [self valueDisplayText];
